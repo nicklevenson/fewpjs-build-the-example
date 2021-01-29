4936aa8fd9fa082a0b91606fc4487a0dd8bb965b
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-28 16:36:13 -0800

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeCases = {
+  '♡' : '♥',
+  '♥' : '♡'
+}
+let errors = document.getElementById("modal")
+let likes = document.getElementsByClassName("like-glyph")
+console.log(likes)
+for (let like of likes) {
+  like.addEventListener("click", likeFunction)
+}
 
-
-
+function likeFunction(event) {
+  console.log(event.target.innerText)
+  mimicServerCall()
+  .then(function(response) {
+    event.target.innerText = likeCases[event.target.innerText]
+    if(event.target.innerText == '♥') {
+      event.target.className = ' activated-heart'
+    }else{
+      event.target.className = ''
+    }
+    return response.json
+  })
+  .catch(function(error) {
+    console.log("ERRORS")
+    errors.className = ""
+    setTimeout(function(){
+      errors.className = "hidden"
+    },5000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
